87b3c42e0f4a157a8f0cd8ac153259e0e4959daa
Rajani G M <109145039+Rajanigm12345@users.noreply.github.com>
2022-07-12 16:42:11 +0530

    Update streamlit_app.py
    correcting of typo mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('Hello my first streamlit')
 
 streamlit.title('I want to learn new languages')
 
-streamlt.title('I want to learn from my mistakes')
+streamlit.title('I want to learn from my mistakes')
