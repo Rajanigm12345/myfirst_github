617b570e949e05eb708d90f0be9bc404271c7daf
Rajani G M <109145039+Rajanigm12345@users.noreply.github.com>
2022-07-12 16:44:51 +0530

    Update streamlit_app.py
    Added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title('Hello my first streamlit')
 streamlit.title('I want to learn new languages')
 
 streamlit.title('I want to learn from my mistakes')
+
+streamlit.title('How to add emojis😊🦊')
