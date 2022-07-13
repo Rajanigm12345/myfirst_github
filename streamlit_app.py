b54511e56f619d7db0c3fa78fb363bc87556c7ff
Rajani G M <109145039+Rajanigm12345@users.noreply.github.com>
2022-07-13 11:56:38 +0530

    Update streamlit_app.py
    Added fruit List

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ streamlit.title('I want to learn new languages')
 streamlit.title('I want to learn from my mistakes')
 
 streamlit.title('How to add emojis😊🦊')
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+streamlit.dataframe(my_fruit_list)
