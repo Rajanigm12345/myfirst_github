af6410b162867e470ef64b92f642052bd93ccae4
Rajani G M <109145039+Rajanigm12345@users.noreply.github.com>
2022-08-10 16:35:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+
+import snowflake.connector
