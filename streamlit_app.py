ba6848755c518c9559d48a058db820de1657735d
Rajani G M <109145039+Rajanigm12345@users.noreply.github.com>
2022-08-10 17:22:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-import requirments.txt
+import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
